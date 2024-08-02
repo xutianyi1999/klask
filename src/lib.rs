9306fe6d82451191a816b35d4618e93212cf6e46
xutianyi <xutianyi1999@live.com>
2024-08-02 11:30:05 +0800

    upgrade dependencies

--- src/lib.rs
+++ src/lib.rs
@@ -100,7 +100,7 @@ pub fn run_app(app: Command, settings: Settings, f: impl FnOnce(&ArgMatches)) {
             native_options,
             Box::new(|cc| {
                 klask.setup(cc);
-                Box::new(klask)
+                Ok(Box::new(klask))
             }),
         )
         .unwrap();
